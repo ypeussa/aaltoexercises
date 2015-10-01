33674049e81f28216399a8d92190a12a5c09b0c9
unknown <vieras@peli13pc.opisto.hkro>
2015-10-01 17:44:52 +0300

    LoopExercise5 done

--- AaltoExercises 9-30/Assets/Scripts/LoopExercise5.cs
+++ AaltoExercises 9-30/Assets/Scripts/LoopExercise5.cs
@@ -4,12 +4,24 @@ using System.Collections;
 public class LoopExercise5 : MonoBehaviour {
 
 	void Start () {
-	
 		// 1) write a for-loop that prints 3, 4, 5, 6
+
 
 
 		// 2) write a for-loop that prints 4, 6, 8
 
 
+
+		// 3) write a for-loop that prints 3, 2, 1
+
+
+
+		// 4) make this while loop print 3, 2, 1. you are only allowed to change the row marked with "XXXXX"
+
+		int i = 0;
+		while (i < 3) {
+			// XXXXX
+			i = i + 1;
+		}
 	}
 }
