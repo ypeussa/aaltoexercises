d3a38d21cbe1930dd0e7b1e285d440ca9a0c422f
Yrjö Peussa <peussa@iki.fi>
2015-10-08 00:39:24 +0300

    Cannon

---
+++ AaltoExercises 9-30/Assets/Scripts/Cannon.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cannon : MonoBehaviour {
+
+	public GameObject projectile;
+	public float projectileLaunchSpeed = 10.0f;
+
+	ParticleSystem muzzleBlast;
+
+	void Start () {
+		muzzleBlast = transform.FindChild("MuzzleBlast").GetComponent<ParticleSystem>();
+	}
+	
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			Fire();
+		}
+
+	}
+
+	public void Fire() {
+		muzzleBlast.Emit(100);
+		var go = Instantiate<GameObject>(projectile);
+		go.transform.position = muzzleBlast.transform.position;
+		go.transform.rotation = Quaternion.LookRotation(transform.forward);
+		go.GetComponent<Rigidbody>().velocity = transform.forward * projectileLaunchSpeed;
+		Destroy (go, 3.0f);
+	}
+}
