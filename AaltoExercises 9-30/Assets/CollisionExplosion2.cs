906e11e695d9b7cbc865bf5a00914c9a04b2cfbf
unknown <yrjo.peussa@peli13pc.opisto.hkro>
2015-10-09 09:17:37 +0300

    cannon exercise about complete

---
+++ AaltoExercises 9-30/Assets/CollisionExplosion2.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionExplosion2 : MonoBehaviour {
+    bool exploded = false;
+
+    void onCollisionEnter(Collision c) {
+        if (c.gameObject.name != "Ground plane") {
+            if (!exploded) {
+                GetComponent<ParticleSystem>().Emit(200);
+                exploded = true;
+            }
+        }
+    }
+}
