9b8493cb06ca51fc4930e45e527b5fbede66da8a
unknown <yrjo.peussa@peli13pc.opisto.hkro>
2015-10-16 14:25:47 +0300

    Stealth project final - line of sight, maximum range + angle, 2-point patrolling

---
+++ stealth/Assets/Chase.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chase : MonoBehaviour {
+    public Transform target;
+
+    public bool seesTarget;
+    public float sightRange;
+    public float sightAngleRange;
+
+    public bool hasSightRange;
+    public bool hasSightAngleRange;
+
+	void Update () {
+        float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+        int layerMask = ~(1 << 9); // do not hit characters (layer 9) but hit everything else
+        bool hit = Physics.Raycast(transform.position,
+            (target.position + Vector3.up - transform.position),
+            distanceToTarget,
+            layerMask);
+        seesTarget = !hit;
+
+        if (hasSightRange && distanceToTarget > sightRange) {
+            seesTarget = false;
+        }
+        if (hasSightAngleRange &&
+            Vector3.Angle(transform.forward, target.position - transform.position) > sightAngleRange) {
+            seesTarget = false;
+        }
+
+        if (seesTarget) {
+            GetComponent<NavMeshAgent>().destination = target.position;
+        }
+
+    }
+
+    void OnDrawGizmos() {
+        if (hasSightRange) {
+            Gizmos.DrawWireSphere(transform.position, sightRange);
+
+        }
+    }
+}
---
+++ stealth/Assets/Patrol.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Patrol : MonoBehaviour {
+    public Transform waypoint1;
+    public Transform waypoint2;
+
+    bool goingTowardFirst;
+    NavMeshAgent nav;
+
+	// Use this for initialization
+	void Start () {
+        nav = GetComponent<NavMeshAgent>();
+        nav.destination = waypoint2.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 currentDestination = goingTowardFirst ? waypoint1.position : waypoint2.position;
+	    if (Vector3.Distance(transform.position, currentDestination) < 1.0f) {
+            goingTowardFirst = !goingTowardFirst;
+            nav.destination = goingTowardFirst ? waypoint1.position : waypoint2.position;
+        }
+	}
+}
